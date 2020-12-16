629233d02114c9556191f0a594f8658feaa5f8df
Alessandro <internicola.alessandro@gmail.com>
2020-12-16 15:11:14 +0100

    Aggiunta degli elementi
    Inserita la possibilità di inserire le task dento la lista tramite vue, usato keyup per l'input, aggiunto un po' di Css e collegato tramite v-model e v-for i data di VueJs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,20 @@
-let app = new Vue({
+// Funzionalitá:
+//
+// - L'utente puó inserire nuove tasks Cliccando sulla "X"
+// - L'utente puó cancellare una task Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
+// - Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera.
+// - Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri.
+
+
+
+let root = new Vue({
 
   el: "#root",
 
   data: {
+
+    newTask: "",
+    list: []
 
   },
 
@@ -12,6 +24,10 @@ let app = new Vue({
 
   methods: {
 
+    submitTask: function(){
+      this.list.push(this.newTask);
+      this.newTask = ""
+    }
 
 }
 
